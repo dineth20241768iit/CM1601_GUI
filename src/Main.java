10a3c00b8cb2e67b4116c5d66f3a1d2d90be781c
DinethIIT <dineth.20241768@iit.ac.lk>
2026-04-18 13:14:42 +0530

    placeholder Main code for JavaFX config

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("CM1601 GUI");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
